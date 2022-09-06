9b1228a76329855c4d9c584759077a7d623b479b
EC2 Default User <ec2-user@ip-172-31-81-17.ec2.internal>
2022-09-06 13:42:14 +0000

    feat: add circle ci config and output txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=geniusapc/develop
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run develop --image=$dockerpath --port=80 --labels app=develop
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward develop 8000:80
